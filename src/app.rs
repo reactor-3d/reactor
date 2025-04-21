c9c91bf0577ca3d405ddb8e8581b1ad721bdf180
XX <freecoder.xx@gmail.com>
2025-04-21 20:43:56 +0300

    feat: add initial egui app with tabs

---
+++ src/app.rs
@@ -0,0 +1,272 @@
+use eframe::{App, CreationContext};
+use egui::ahash::HashSet;
+use egui::{Id, Key, LayerId, Order, Sense, UiBuilder};
+use egui_dock::{AllowedSplits, DockArea, DockState, SurfaceIndex, TabViewer};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Copy, Clone, Deserialize, Serialize, egui_probe::EguiProbe)]
+pub enum EditMode {
+    Editing,
+    View,
+}
+
+impl EditMode {
+    pub fn switch(&mut self) -> Self {
+        match self {
+            Self::Editing => *self = Self::View,
+            Self::View => *self = Self::Editing,
+        }
+        *self
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, egui_probe::EguiProbe)]
+pub struct TabsSettings {
+    pub draggable_tabs: bool,
+    pub show_tab_name_on_hover: bool,
+
+    #[serde(skip)]
+    #[egui_probe(skip)]
+    pub allowed_splits: AllowedSplits,
+
+    pub show_close_buttons: bool,
+    pub show_add_buttons: bool,
+    pub show_leaf_close_all_buttons: bool,
+    pub show_leaf_collapse_buttons: bool,
+    pub show_secondary_button_hint: bool,
+    pub secondary_button_on_modifier: bool,
+    pub secondary_button_context_menu: bool,
+
+    #[egui_probe(skip)]
+    pub style: Option<egui_dock::Style>,
+}
+
+impl Default for TabsSettings {
+    fn default() -> Self {
+        Self {
+            draggable_tabs: true,
+            show_tab_name_on_hover: true,
+            allowed_splits: AllowedSplits::All,
+            show_close_buttons: true,
+            show_add_buttons: true,
+            show_leaf_close_all_buttons: true,
+            show_leaf_collapse_buttons: true,
+            show_secondary_button_hint: true,
+            secondary_button_on_modifier: false,
+            secondary_button_context_menu: false,
+            style: None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, egui_probe::EguiProbe)]
+pub struct AppSettings {
+    pub edit_mode: EditMode,
+    pub editing_nodes_opacity: f32,
+    pub viewing_nodes_opacity: f32,
+    pub show_nodes: bool,
+    pub animation_time: f32,
+    pub tabs: TabsSettings,
+}
+
+impl Default for AppSettings {
+    fn default() -> Self {
+        Self {
+            edit_mode: EditMode::Editing,
+            editing_nodes_opacity: 1.0,
+            viewing_nodes_opacity: 0.5,
+            show_nodes: true,
+            animation_time: 0.2,
+            tabs: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
+pub enum Tab {
+    Viewport,
+    Settings,
+}
+
+impl Tab {
+    pub fn title(self) -> &'static str {
+        match self {
+            Self::Viewport => "Viewport",
+            Self::Settings => "Settings",
+        }
+    }
+}
+
+pub struct AppContext {
+    open_tabs: HashSet<Tab>,
+    settings: AppSettings,
+}
+
+impl TabViewer for AppContext {
+    type Tab = Tab;
+
+    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
+        tab.title().into()
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
+        match tab {
+            Tab::Viewport => {
+                ui.input(|i| {
+                    if i.key_pressed(Key::Tab) {
+                        self.settings.edit_mode.switch();
+                    }
+                    if i.key_pressed(Key::H) {
+                        self.settings.show_nodes = !self.settings.show_nodes;
+                    }
+                });
+
+                let last_panel_rect = ui.min_rect();
+
+                // Render area in the background
+                let render_area_ui = ui.new_child(
+                    UiBuilder::new()
+                        .layer_id(LayerId::new(Order::Background, Id::new("render_area")))
+                        .max_rect(last_panel_rect)
+                        .sense(Sense::empty()),
+                );
+
+                if let EditMode::View = self.settings.edit_mode {
+                    // Overlay mouse blocker in the foreground
+                    let overlay_area_ui = ui.new_child(
+                        UiBuilder::new()
+                            .layer_id(LayerId::new(Order::Foreground, Id::new("overlay_area")))
+                            .max_rect(last_panel_rect)
+                            .sense(Sense::empty()),
+                    );
+                    let overlay_response =
+                        overlay_area_ui.interact(last_panel_rect, Id::new("overlay_blocker"), Sense::click_and_drag());
+                }
+            },
+            Tab::Settings => {
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    egui_probe::Probe::new(&mut self.settings).show(ui);
+                });
+            },
+        }
+    }
+
+    fn on_close(&mut self, tab: &mut Self::Tab) -> bool {
+        self.open_tabs.remove(tab);
+        true
+    }
+}
+
+pub struct Reactor3dApp {
+    ctx: AppContext,
+    tabs_tree: DockState<Tab>,
+}
+
+impl Reactor3dApp {
+    pub fn new(cx: &CreationContext) -> Self {
+        egui_extras::install_image_loaders(&cx.egui_ctx);
+
+        cx.egui_ctx.style_mut(|style| style.animation_time *= 10.0);
+
+        let settings = cx.storage.map_or_else(AppSettings::default, |storage| {
+            storage
+                .get_string("settings")
+                .and_then(|settings| serde_json::from_str(&settings).ok())
+                .unwrap_or_default()
+        });
+
+        let tabs_tree = cx
+            .storage
+            .and_then(|storage| {
+                storage
+                    .get_string("tabs_tree")
+                    .and_then(|tabs_tree| serde_json::from_str(&tabs_tree).ok())
+            })
+            .unwrap_or_else(|| DockState::<Tab>::new(Default::default()));
+
+        let mut open_tabs = HashSet::default();
+        for node in tabs_tree[SurfaceIndex::main()].iter() {
+            if let Some(tabs) = node.tabs() {
+                for tab in tabs {
+                    open_tabs.insert(*tab);
+                }
+            }
+        }
+
+        Self {
+            ctx: AppContext { open_tabs, settings },
+            tabs_tree,
+        }
+    }
+}
+
+impl App for Reactor3dApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                    }
+                });
+                ui.add_space(16.0);
+
+                ui.menu_button("View", |ui| {
+                    // allow certain tabs to be toggled
+                    for tab in [Tab::Viewport, Tab::Settings] {
+                        if ui
+                            .selectable_label(self.ctx.open_tabs.contains(&tab), tab.title())
+                            .clicked()
+                        {
+                            if let Some(index) = self.tabs_tree.find_tab(&tab) {
+                                self.tabs_tree.remove_tab(index);
+                                self.ctx.open_tabs.remove(&tab);
+                            } else {
+                                self.tabs_tree[SurfaceIndex::main()].push_to_focused_leaf(tab);
+                            }
+
+                            ui.close_menu();
+                        }
+                    }
+                });
+                ui.add_space(16.0);
+
+                egui::widgets::global_theme_preference_switch(ui);
+            });
+        });
+
+        ctx.style_mut(|style| style.animation_time = self.ctx.settings.animation_time);
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let style = self
+                .ctx
+                .settings
+                .tabs
+                .style
+                .get_or_insert(egui_dock::Style::from_egui(ui.style()))
+                .clone();
+
+            DockArea::new(&mut self.tabs_tree)
+                .style(style)
+                .show_close_buttons(self.ctx.settings.tabs.show_close_buttons)
+                .show_add_buttons(self.ctx.settings.tabs.show_add_buttons)
+                .draggable_tabs(self.ctx.settings.tabs.draggable_tabs)
+                .show_tab_name_on_hover(self.ctx.settings.tabs.show_tab_name_on_hover)
+                .allowed_splits(self.ctx.settings.tabs.allowed_splits)
+                .show_leaf_close_all_buttons(self.ctx.settings.tabs.show_leaf_close_all_buttons)
+                .show_leaf_collapse_buttons(self.ctx.settings.tabs.show_leaf_collapse_buttons)
+                .show_secondary_button_hint(self.ctx.settings.tabs.show_secondary_button_hint)
+                .secondary_button_on_modifier(self.ctx.settings.tabs.secondary_button_on_modifier)
+                .secondary_button_context_menu(self.ctx.settings.tabs.secondary_button_context_menu)
+                .show_inside(ui, &mut self.ctx);
+        });
+    }
+
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        let settings = serde_json::to_string(&self.ctx.settings).unwrap();
+        storage.set_string("settings", settings);
+
+        let tabs_tree = serde_json::to_string(&self.tabs_tree).unwrap();
+        storage.set_string("tabs_tree", tabs_tree);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use std::sync::Arc;
+
+use eframe::egui_wgpu::{WgpuConfiguration, WgpuSetup, WgpuSetupCreateNew};
+use eframe::wgpu;
+
+use self::app::Reactor3dApp;
+
+mod app;
+
+fn main() -> eframe::Result<()> {
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([800.0, 600.0])
+            .with_min_inner_size([700.0, 520.0]),
+        wgpu_options: WgpuConfiguration {
+            wgpu_setup: WgpuSetup::CreateNew(WgpuSetupCreateNew {
+                device_descriptor: Arc::new(|adapter| {
+                    let mut base_limits = if adapter.get_info().backend == wgpu::Backend::Gl {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    };
+                    base_limits.max_storage_buffer_binding_size = 512 << 20;
+
+                    wgpu::DeviceDescriptor {
+                        label: Some("egui wgpu device"),
+                        required_features: wgpu::Features::default(),
+                        required_limits: wgpu::Limits {
+                            // When using a depth buffer, we have to be able to create a texture
+                            // large enough for the entire surface, and we want to support 4k+ displays.
+                            max_texture_dimension_2d: 8192,
+                            ..base_limits
+                        },
+                        memory_hints: wgpu::MemoryHints::default(),
+                    }
+                }),
+                ..Default::default()
+            }),
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "reactor-3d",
+        native_options,
+        Box::new(|cx| Ok(Box::new(Reactor3dApp::new(cx)))),
+    )
+}
